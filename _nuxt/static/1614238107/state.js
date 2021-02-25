06df11a2f4865d9a97da720482dbcb0ff4f60ca4
dangcq-1093 <78715700+dangcq-1093@users.noreply.github.com>
2021-02-25 14:31:43 +0700

    Add files via upload

---
+++ _nuxt/static/1614238107/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxtjs\u002F_nuxt\u002Fstatic\u002F1614238107",layout:"default",error:a,state:{todos:{todoList:[]}},serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fnuxtjs\u002F",assetsPath:"\u002Fnuxtjs\u002F_nuxt\u002F",cdnURL:a}}}}(null));
